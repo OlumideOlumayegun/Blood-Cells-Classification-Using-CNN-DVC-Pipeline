0a175167701c56c4a0a59d63a9d8b83ee0de8edf
Olumide Olumayegun <midemayegun@outlook.com>
2024-10-31 21:48:30 +0000

    model evaluation completed

---
+++ src/bloodcellClassifier/pipeline/stage_04_evaluation.py
@@ -0,0 +1,33 @@
+from bloodcellClassifier.config.configuration import ConfigurationManager
+from bloodcellClassifier.components.evaluation import Evaluation
+from bloodcellClassifier import logger
+
+
+
+
+STAGE_NAME = "Evaluation stage"
+
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        val_config = config.get_validation_config()
+        evaluation = Evaluation(val_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
